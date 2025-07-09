80480dd6147ab0317fe7c0db0d65e3976f4ffcd3
maxinth <tunde.j.adeleye@gmail.com>
2025-07-09 15:59:13 +0100

    add setup for tests and test for nav

---
+++ src/__tests__/navbar.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "../components/Navbar";
+
+//* Mock for imported image
+jest.mock("../assets/BLOTT.png", () => "blott-logo.png");
+
+describe("Navbar", () => {
+  it("renders without crashing", () => {
+    render(<Navbar />);
+    const navElement = screen.getByRole("navigation");
+
+    expect(navElement).toBeDefined();
+  });
+
+  it("renders the logo image with correct alt text", () => {
+    render(<Navbar />);
+    const logoImage = screen.getByAltText("logo");
+
+    expect(logoImage).toBeDefined();
+    expect(logoImage.tagName).toBe("IMG");
+  });
+});
